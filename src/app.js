19da0093192e23af61aae8cac7f173e37d270c85
vinay diwan <vinaydwn99@gmail.com>
2021-09-04 20:12:43 +0530

    1.4.1

--- src/app.js
+++ src/app.js
@@ -64,7 +64,6 @@ app.use(session({
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-        secure: true,
     },
 }));
 app.use(flash());
